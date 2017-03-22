16f294a1ba1f0ca317ad42e938dcaff201237d57
gaokangkang <gaokangkang>
2017-03-22 14:58:54 +0800

    导入冻存管

--- src/main/webapp/app/admin/transport-record/frozenStorageBoxModalController.js
+++ src/main/webapp/app/admin/transport-record/frozenStorageBoxModalController.js
@@ -8,9 +8,23 @@
         .module('bioBankApp')
         .controller('FrozenStorageBoxModalController', FrozenStorageBoxModalController);
 
-    FrozenStorageBoxModalController.$inject = ['DTOptionsBuilder','DTColumnBuilder','$uibModal'];
+    FrozenStorageBoxModalController.$inject = ['DTOptionsBuilder','DTColumnBuilder','$uibModalInstance','$uibModal'];
 
-    function FrozenStorageBoxModalController(DTOptionsBuilder,DTColumnBuilder,$uibModal) {
+    function FrozenStorageBoxModalController(DTOptionsBuilder,DTColumnBuilder,$uibModalInstance,$uibModal) {
+        this.dtOptions = DTOptionsBuilder.fromSource('app/admin/transport-record/data.json')
+            .withPaginationType('full_numbers')
+            .withOption('searching', false);
+        this.dtColumns = [
+            DTColumnBuilder.newColumn('id').withTitle('ID'),
+            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+        ];
 
+        this.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+        this.ok = function () {
+            $uibModalInstance.close();
+        };
     }
 })();

118a31919058e98c0e1cf2f3a085706f17c14c72
zhuyu <yu.zhu@fwoxford.org>
2017-04-25 14:01:55 +0800

    For jq datatable的汉化

--- src/main/webapp/app/blocks/handlers/translation.handler.js
+++ src/main/webapp/app/blocks/handlers/translation.handler.js
@@ -5,9 +5,9 @@
         .module('bioBankApp')
         .factory('translationHandler', translationHandler);
 
-    translationHandler.$inject = ['$rootScope', '$window', '$state', '$translate'];
+    translationHandler.$inject = ['$rootScope', '$window', '$state', '$translate', 'DTDefaultOptions'];
 
-    function translationHandler($rootScope, $window, $state, $translate) {
+    function translationHandler($rootScope, $window, $state, $translate, DTDefaultOptions) {
         return {
             initialize: initialize,
             updateTitle: updateTitle
@@ -24,6 +24,25 @@
                     translateChangeSuccess();
                 }
             });
+
+            var tableLang = {
+                'emptyTable': '没有数据',
+                'loadingRecords': '加载中...',
+                'processing': '查询中...',
+                'search': '搜索:',
+                'lengthMenu': '每页 _MENU_ 条',
+                'zeroRecords': '没有数据',
+                'paginate': {
+                    'first':      '首页',
+                    'last':       '末页',
+                    'next':       '下一页',
+                    'previous':   '上一页'
+                },
+                'info': '第 _PAGE_ 页 / 总 _PAGES_ 页',
+                'infoEmpty': '没有数据',
+                'infoFiltered': '(过滤后总行数 _MAX_ 条)'
+            };
+            DTDefaultOptions.setLanguage(tableLang);
         }
 
         // update the window title using params in the following

14482864ddbcd1105864df22a409f9f068a60e53
gaokangkang <gaokangkang>
2017-05-04 16:29:35 +0800

    修改样本类型

--- src/main/webapp/app/bizs/transport-record/frozenStorageBoxModalController.js
+++ src/main/webapp/app/bizs/transport-record/frozenStorageBoxModalController.js
@@ -9,39 +9,48 @@
         .controller('FrozenStorageBoxModalController', FrozenStorageBoxModalController)
         .controller('ModalInstanceCtrl', ModalInstanceCtrl);
 
-    FrozenStorageBoxModalController.$inject = ['$scope','toastr','$timeout','DTOptionsBuilder','DTColumnBuilder','$uibModalInstance','$uibModal','items','TranshipBoxService','blockUI','AreasByEquipmentIdService','EquipmentService'];
+    FrozenStorageBoxModalController.$inject = ['$scope','toastr','$timeout','DTOptionsBuilder','DTColumnBuilder','$uibModalInstance','$uibModal','items','TranshipBoxService','blockUI','AreasByEquipmentIdService','EquipmentService','SampleTypeService'];
     ModalInstanceCtrl.$inject = ['$uibModalInstance','$uibModal'];
 
-    function FrozenStorageBoxModalController($scope,toastr,$timeout,DTOptionsBuilder,DTColumnBuilder,$uibModalInstance,$uibModal,items,TranshipBoxService,blockUI,AreasByEquipmentIdService,EquipmentService) {
+    function FrozenStorageBoxModalController($scope,toastr,$timeout,DTOptionsBuilder,DTColumnBuilder,$uibModalInstance,$uibModal,items,TranshipBoxService,blockUI,AreasByEquipmentIdService,EquipmentService,SampleTypeService) {
 
         var vm = this;
         vm.items = items;
-        vm.importSample = importSample;//导入样本数据
+        // vm.importSample = importSample;//导入样本数据
         vm.codeList = [];//扫码录入的盒号
         vm.obox = {
             transhipId:vm.items.transhipId,
             frozenBoxDTOList:[]
         };
+        vm.sampleTypeOptions = items.sampleTypeOptions;
+        vm.frozenBoxTypeOptions = items.frozenBoxTypeOptions;
         vm.frozenBox = {};
-        var sampleTypeOptions = _.orderBy(items.sampleTypeOptions, ['sampleTypeCode'], ['esc']);
-        var frozenBoxTypeOptions = _.orderBy(items.frozenBoxTypeOptions, ['id'], ['esc']);
-        vm.frozenBoxTypeOptions = frozenBoxTypeOptions;
-        vm.sampleTypeOptions = sampleTypeOptions;
         vm.frozenBox.frozenBoxTypeId = vm.frozenBoxTypeOptions[0].id;
         vm.frozenBox.frozenBoxTypeRows = vm.frozenBoxTypeOptions[0].frozenBoxTypeRows;
         vm.frozenBox.frozenBoxTypeColumns = vm.frozenBoxTypeOptions[0].frozenBoxTypeColumns;
-        vm.frozenBox.sampleTypeId = vm.sampleTypeOptions[0].id;
-        vm.frozenBox.sampleTypeCode = vm.sampleTypeOptions[0].sampleTypeCode;
+        if(vm.sampleTypeOptions.length){
+            vm.frozenBox.sampleTypeId = vm.sampleTypeOptions[0].sampleTypeId;
+            _fnQueryProjectSampleClass(vm.items.projectId,vm.frozenBox.sampleTypeId)
+        }
         //设备
         EquipmentService.query({},onEquipmentTempSuccess, onError);
         //样本类型
         vm.sampleTypeConfig = {
-            valueField:'sampleTypeCode',
+            valueField:'sampleTypeId',
             labelField:'sampleTypeName',
             maxItems: 1,
             onChange:function (value) {
-                vm.frozenBox.sampleTypeCode = value;
+                vm.frozenBox.sampleTypeId = value;
                 _fnInitBoxInfo();
+                _fnQueryProjectSampleClass(vm.items.projectId,value)
+            }
+        };
+        vm.projectSampleTypeConfig = {
+            valueField:'sampleClassificationId',
+            labelField:'sampleClassficationName',
+            maxItems: 1,
+            onChange:function (value) {
+
             }
         };
         //盒类型
@@ -60,6 +69,7 @@
                 _fnInitBoxInfo();
             }
         };
+        //暂存区位置
         vm.frozenBoxPlaceConfigTemp = {
             valueField:'id',
             labelField:'equipmentCode',
@@ -85,7 +95,8 @@
             .withScroller()
             .withOption('deferRender', true)
             .withOption('scrollY', 300);
-        vm.myConfig = {
+        //录入冻存盒号
+        vm.boxCodeConfig = {
             create: true,
             persist:false,
             onChange: function(value){
@@ -97,19 +108,19 @@
                 $scope.$apply();
             }
         };
-        function importSample() {
-            var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: 'stackedModal.html',
-                size: 'sm',
-                controller: 'ModalInstanceCtrl',
-                controllerAs: 'ctrl'
-            });
-            modalInstance.result.then(function (flag) {
-
-            }, function () {
-            });
-        }
+        // function importSample() {
+        //     var modalInstance = $uibModal.open({
+        //         animation: true,
+        //         templateUrl: 'stackedModal.html',
+        //         size: 'sm',
+        //         controller: 'ModalInstanceCtrl',
+        //         controllerAs: 'ctrl'
+        //     });
+        //     modalInstance.result.then(function (flag) {
+        //
+        //     }, function () {
+        //     });
+        // }
         this.cancel = function () {
             $uibModalInstance.dismiss('cancel');
         };
@@ -119,13 +130,18 @@
             TranshipBoxService.save(vm.obox,onSaveBoxSuccess,onError);
 
         };
+        //不同项目下的样本分类
+        function _fnQueryProjectSampleClass(projectId,sampleTypeId) {
+            SampleTypeService.queryProjectSampleClasses(projectId,sampleTypeId).success(function (data) {
+                vm.projectSampleTypeOptions = data;
+                vm.frozenBox.sampleClassificationId = vm.projectSampleTypeOptions[0].sampleClassificationId
+            });
+        }
         function _fnInitBoxInfo() {
             var tubeList=[];
             for(var i = 0; i < vm.codeList.length; i++){
                 vm.obox.frozenBoxDTOList[i] = {
                     frozenBoxCode:vm.codeList[i],
-                    isSplit: 0,//是否分装:'否:0003 是：0002',
-                    status: "2001",//状态
                     sampleTypeId:vm.frozenBox.sampleTypeId,
                     frozenBoxTypeId:vm.frozenBox.frozenBoxTypeId,
                     equipmentId:vm.frozenBox.equipmentId,
@@ -137,12 +153,11 @@
                     for(var k = 0; k < vm.frozenBox.frozenBoxTypeColumns; k++){
                         tubeList[j][k] = {
                             frozenBoxCode: vm.codeList[i],
-                            frozenTubeCode:vm.codeList[i]+j,
                             sampleCode: "",
                             sampleTempCode: vm.codeList[i]+"-"+String.fromCharCode(j+65)+(k+1),
-                            sampleTypeCode: vm.frozenBox.sampleTypeCode,
+                            sampleTypeId: vm.frozenBox.sampleTypeId,
+                            sampleClassificationId:vm.frozenBox.sampleClassificationId,
                             status: "3001",
-                            memo:"",
                             tubeRows:String.fromCharCode(j+65),
                             tubeColumns: k+1
                         };

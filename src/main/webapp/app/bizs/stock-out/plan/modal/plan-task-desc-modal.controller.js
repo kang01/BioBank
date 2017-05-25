98392e5705e8f672d0633e2f54fad888755c81e0
gaokangkang <kangkang.gao@fwoxford.org>
2017-05-25 16:20:26 +0800

    申请页面

--- src/main/webapp/app/bizs/stock-out/plan/modal/plan-task-desc-modal.controller.js
+++ src/main/webapp/app/bizs/stock-out/plan/modal/plan-task-desc-modal.controller.js
@@ -9,10 +9,61 @@
         .module('bioBankApp')
         .controller('PlanTaskDescModalController', PlanTaskDescModalController);
 
-    PlanTaskDescModalController.$inject = ['$uibModalInstance','$uibModal','items'];
+    PlanTaskDescModalController.$inject = ['$uibModalInstance','$uibModal','items','DTColumnBuilder','DTOptionsBuilder','PlanService'];
 
-    function PlanTaskDescModalController($uibModalInstance,$uibModal,items) {
+    function PlanTaskDescModalController($uibModalInstance,$uibModal,items,DTColumnBuilder,DTOptionsBuilder,PlanService) {
         var vm = this;
+        var taskId = items.taskId;
+        //盒子列表
+        vm.dtOptions = DTOptionsBuilder.newOptions()
+            .withOption('processing',true)
+            .withOption('serverSide',true)
+            .withFnServerData(function ( sSource, aoData, fnCallback, oSettings ) {
+                var data = {};
+                for(var i=0; aoData && i<aoData.length; ++i){
+                    var oData = aoData[i];
+                    data[oData.name] = oData.value;
+                }
+                var jqDt = this;
+
+                PlanService.queryTaskBoxesDes(taskId,data).then(function (res){
+                        vm.selectAll = false;
+                        vm.selected = {};
+                        var json = res.data;
+                        var error = json.error || json.sError;
+                        if ( error ) {
+                            jqDt._fnLog( oSettings, 0, error );
+                        }
+                        oSettings.json = json;
+                        fnCallback( json );
+                    }).catch(function(res){
+                        // console.log(res);
+                        //
+                        var ret = jqDt._fnCallbackFire( oSettings, null, 'xhr', [oSettings, null, oSettings.jqXHR] );
+
+                        if ( $.inArray( true, ret ) === -1 ) {
+                            if ( error == "parsererror" ) {
+                                jqDt._fnLog( oSettings, 0, 'Invalid JSON response', 1 );
+                            }
+                            else if ( res.readyState === 4 ) {
+                                jqDt._fnLog( oSettings, 0, 'Ajax error', 7 );
+                            }
+                        }
+                        jqDt._fnProcessingDisplay( oSettings, false );
+                    });
+
+
+            })
+            .withPaginationType('full_numbers')
+
+        vm.dtColumns = [
+            DTColumnBuilder.newColumn('frozenBoxCode').withTitle('冻存盒编码'),
+            DTColumnBuilder.newColumn('sampleTypeName').withTitle('样本类型'),
+            DTColumnBuilder.newColumn('position').withTitle('冻存位置'),
+            DTColumnBuilder.newColumn('countOfSample').withTitle('出库样本数量'),
+            DTColumnBuilder.newColumn('id').notVisible()
+        ];
+
         vm.ok = function () {
             $uibModalInstance.close();
         };

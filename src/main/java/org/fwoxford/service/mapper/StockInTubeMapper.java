63f1929613f3bcf8a43bf8d1ace3f09b10569ccb
GengLuying <luying.geng@fwoxford.org>
2017-07-21 15:57:29 +0800

    入库冻存管历史 xml 实体 创建

--- src/main/java/org/fwoxford/service/mapper/StockInTubeMapper.java
+++ src/main/java/org/fwoxford/service/mapper/StockInTubeMapper.java
@@ -14,12 +14,22 @@ public interface StockInTubeMapper {
 
     @Mapping(source = "stockInBox.id", target = "stockInBoxId")
     @Mapping(source = "frozenTube.id", target = "frozenTubeId")
+    @Mapping(source = "frozenTubeType.id", target = "frozenTubeTypeId")
+    @Mapping(source = "sampleType.id", target = "sampleTypeId")
+    @Mapping(source = "sampleClassification.id", target = "sampleClassificationId")
+    @Mapping(source = "project.id", target = "projectId")
+    @Mapping(source = "projectSite.id", target = "projectSiteId")
     StockInTubeDTO stockInTubeToStockInTubeDTO(StockInTube stockInTube);
 
     List<StockInTubeDTO> stockInTubesToStockInTubeDTOs(List<StockInTube> stockInTubes);
 
     @Mapping(source = "stockInBoxId", target = "stockInBox")
     @Mapping(source = "frozenTubeId", target = "frozenTube")
+    @Mapping(source = "frozenTubeTypeId", target = "frozenTubeType")
+    @Mapping(source = "sampleTypeId", target = "sampleType")
+    @Mapping(source = "sampleClassificationId", target = "sampleClassification")
+    @Mapping(source = "projectId", target = "project")
+    @Mapping(source = "projectSiteId", target = "projectSite")
     StockInTube stockInTubeDTOToStockInTube(StockInTubeDTO stockInTubeDTO);
 
     List<StockInTube> stockInTubeDTOsToStockInTubes(List<StockInTubeDTO> stockInTubeDTOs);
@@ -40,5 +50,45 @@ public interface StockInTubeMapper {
         FrozenTube frozenTube = new FrozenTube();
         frozenTube.setId(id);
         return frozenTube;
+    }
+    default FrozenTubeType frozenTubeTypeFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        FrozenTubeType frozenTubeType = new FrozenTubeType();
+        frozenTubeType.setId(id);
+        return frozenTubeType;
+    }
+    default SampleType sampleTypeFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        SampleType sampleType = new SampleType();
+        sampleType.setId(id);
+        return sampleType;
+    }
+    default Project projectFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Project project = new Project();
+        project.setId(id);
+        return project;
+    }
+    default ProjectSite projectSiteFromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        ProjectSite projectSite = new ProjectSite();
+        projectSite.setId(id);
+        return projectSite;
+    }
+    default SampleClassification sampleClassificationFromId(Long id){
+        if (id == null) {
+            return null;
+        }
+        SampleClassification sampleClassification = new SampleClassification();
+        sampleClassification.setId(id);
+        return sampleClassification;
     }
 }

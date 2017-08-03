e9f09ffe0acaece7261ca2901cdc3213abd11831
gaokangkang <kangkang.gao@fwoxford.org>
2017-08-03 13:50:05 +0800

    修改指定样本详情修改

--- src/main/webapp/app/bizs/stock-out/requirement/modal/requirement-appoint-sample-desc-modal.controller.js
+++ src/main/webapp/app/bizs/stock-out/requirement/modal/requirement-appoint-sample-desc-modal.controller.js
@@ -9,79 +9,39 @@
         .module('bioBankApp')
         .controller('RequirementAppointSampleDescModalController', RequirementAppointSampleDescModalController);
 
-    RequirementAppointSampleDescModalController.$inject = ['$scope','$uibModalInstance','toastr','items','DTOptionsBuilder','DTColumnBuilder','RequirementAppointSampleDescService','BioBankDataTable'];
+    RequirementAppointSampleDescModalController.$inject = ['$scope','$uibModalInstance','toastr','items','PaginationUtil','paginationConstants','DTColumnBuilder','RequirementAppointSampleDescService','BioBankDataTable'];
 
-    function RequirementAppointSampleDescModalController($scope,$uibModalInstance,toastr,items,DTOptionsBuilder,DTColumnBuilder,RequirementAppointSampleDescService,BioBankDataTable) {
+    function RequirementAppointSampleDescModalController($scope,$uibModalInstance,toastr,items,PaginationUtil,paginationConstants,DTColumnBuilder,RequirementAppointSampleDescService,BioBankDataTable) {
         var vm = this;
+        vm.transition = transition;
         //样本需求ID
         var sampleRequirementId = items.sampleRequirementId;
+        //一页30条数据
+        vm.itemsPerPage = 30;
+        //第几页
+        vm.page = 1;
 
+        //加载指定样本详情
         function loadSample() {
             RequirementAppointSampleDescService.query({
                 requirementId:sampleRequirementId,
-                page:0,
-                size: 30
+                page:vm.page-1,
+                size: vm.itemsPerPage
             }, onSuccess, onError);
             function onSuccess(data,headers) {
-                // vm.links = ParseLinks.parse(headers('link'));
                 vm.totalItems = headers('X-Total-Count');
                 vm.queryCount = vm.totalItems;
-                vm.samples = data;
-                // vm.page =1;
+                vm.samples = _.chunk(data,3);
             }
-            function onError(data) {
-                toastr.error(data.message)
-            }
-            // RequirementService.appointDescSampleRequirement(sampleRequirementId).success(function (data,headers) {
-            //     vm.totalItems = headers('X-Total-Count');
-            //     vm.queryCount = vm.totalItems;
-            //     vm.samples = data;
-            //    console.log(JSON.stringify(data));
-            // });
         }
         loadSample();
+        //每次分页都需要请求数据
+        function transition() {
+            loadSample();
+        }
 
-        vm.dtOptions = BioBankDataTable.buildDTOption("NORMALLY")
-            // .withOption('serverSide',true)
-            // .withFnServerData(_fnServerData);
-
-        vm.dtColumns = [
-            DTColumnBuilder.newColumn('sampleCode').withTitle('样本编码'),
-            DTColumnBuilder.newColumn('sampleType').withTitle('样本类型')
-
-        ];
-        function _fnServerData( sSource, aoData, fnCallback, oSettings ) {
-            var data = {};
-            for(var i=0; aoData && i<aoData.length; ++i){
-                var oData = aoData[i];
-                data[oData.name] = oData.value;
-            }
-            var jqDt = this;
-            RequirementService.appointDescSampleRequirement(sampleRequirementId, data, oSettings).then(function (res){
-                var json = res.data;
-                vm.len = _.filter(res.data.data,{status:'3001'}).length;
-                var error = json.error || json.sError;
-                if ( error ) {
-                    jqDt._fnLog( oSettings, 0, error );
-                }
-                oSettings.json = json;
-                fnCallback( json );
-            }, function(res){
-                console.log(res);
-
-                var ret = jqDt._fnCallbackFire( oSettings, null, 'xhr', [oSettings, null, oSettings.jqXHR] );
-
-                if ( $.inArray( true, ret ) === -1 ) {
-                    if ( error == "parsererror" ) {
-                        jqDt._fnLog( oSettings, 0, 'Invalid JSON response', 1 );
-                    }
-                    else if ( res.readyState === 4 ) {
-                        jqDt._fnLog( oSettings, 0, 'Ajax error', 7 );
-                    }
-                }
-
-                jqDt._fnProcessingDisplay( oSettings, false );
-            });
+        function onError(data) {
+            toastr.error(data.message)
         }
 
         // vm.ok = function () {

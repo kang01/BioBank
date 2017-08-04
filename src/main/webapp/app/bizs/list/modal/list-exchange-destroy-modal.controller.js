4af358ef08ed8a8dd89de279952e61dd272129d6
gaokangkang <kangkang.gao@fwoxford.org>
2017-08-04 13:02:37 +0800

    清单换位、销毁修改

--- src/main/webapp/app/bizs/list/modal/list-exchange-destroy-modal.controller.js
+++ src/main/webapp/app/bizs/list/modal/list-exchange-destroy-modal.controller.js
@@ -9,15 +9,47 @@
         .module('bioBankApp')
         .controller('ListExchangeDestroyModalController', ListExchangeDestroyModalController);
 
-    ListExchangeDestroyModalController.$inject = ['$uibModalInstance','$uibModal','items'];
+    ListExchangeDestroyModalController.$inject = ['$uibModalInstance','$uibModal','items','SampleUserService','toastr','Principal'];
 
-    function ListExchangeDestroyModalController($uibModalInstance,$uibModal,items) {
+    function ListExchangeDestroyModalController($uibModalInstance,$uibModal,items,SampleUserService,toastr,Principal) {
 
         var vm = this;
         //1.交换 2.销毁
         vm.operateStatus = items.operateStatus;
+        vm.entity = {};
+        //获取当前用户
+        function _fnQueryCurrentUser() {
+            Principal.identity().then(function(account) {
+                vm.account = account;
+                vm.entity.userId = vm.account.id;
+            });
+        }
+        function _loadUser() {
+            vm.userConfig = {
+                valueField: 'id',
+                labelField: 'userName',
+                maxItems: 1,
+                onChange:function (value) {
+                }
+            };
+            //出库负责人
+            SampleUserService.query({}, onUserSuccess, onError);
+            function onUserSuccess(data) {
+                vm.userOptions = data;
+            }
+        }
+        _fnQueryCurrentUser();
+        _loadUser();
+
+
+
+
+        function onError(error) {
+            toastr.error(error.data.message);
+        }
+
         vm.ok = function () {
-            $uibModalInstance.close(vm.reason);
+            $uibModalInstance.close(vm.entity);
         };
         vm.cancel = function () {
             $uibModalInstance.dismiss('cancel');

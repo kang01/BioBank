5e187f1aa8b5e1f43a8a4b0e46bd712f1c1b5b9f
gaokangkang <kangkang.gao@fwoxford.org>
2017-08-21 08:39:01 +0800

    bug修改

--- src/main/webapp/app/services/common/common.service.js
+++ src/main/webapp/app/services/common/common.service.js
@@ -151,6 +151,7 @@
                 {value:"2006",label:"已上架"},
                 {value:"2008",label:"待出库"},
                 {value:"2009",label:"已出库"},
+                {value:"2010",label:"已交接"},
                 {value:"2011",label:"转运完成"}
             ];
             // 冻存管状态

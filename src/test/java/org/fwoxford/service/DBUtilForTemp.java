6ca4d9e9dff7e567ca052180f716eb106257770d
GengLuying <luying.geng@fwoxford.org>
2017-10-11 14:15:55 +0800

    导入数据

--- src/test/java/org/fwoxford/service/DBUtilForTemp.java
+++ src/test/java/org/fwoxford/service/DBUtilForTemp.java
@@ -18,8 +18,8 @@ public class DBUtilForTemp {
 
     static {
         driver="oracle.jdbc.driver.OracleDriver";//需要的数据库驱动
-        url="jdbc:oracle:" + "thin:@10.24.10.56:1521:xe";//数据库名路径
-        username="biobank_temp_0825";
+        url="jdbc:oracle:" + "thin:@10.24.200.38:1521/oracle12c";//数据库名路径
+        username="c##biobank_temp_0825";
         password="root123";
     }
     public static Connection open()

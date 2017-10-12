86cd22cb0940aa5b9a86ace5028590e77f5ee9a9
gengluying <luying.geng@fwoxford.org>
2017-10-12 22:22:59 +0800

    验证冻存管编码重复

--- src/main/java/org/fwoxford/service/FrozenTubeCheckService.java
+++ src/main/java/org/fwoxford/service/FrozenTubeCheckService.java
@@ -37,6 +37,7 @@ public class FrozenTubeCheckService {
         }
         List<FrozenTube> frozenTubeListForCheckRepeat = new ArrayList<FrozenTube>();
         List<FrozenTube> frozenTubeList = frozenTubeRepository.findBySampleCodeInAndProjectCodeAndSampleTypeIdAndStatusNot(sampleCodeStr,frozenBox.getProjectCode(),frozenBox.getSampleType().getId(), Constants.INVALID);
+
         for(FrozenTube tube :frozenTubeList){
             String sampleCode = tube.getSampleCode()!=null?tube.getSampleCode():tube.getSampleTempCode();
             List<String> stringList = new ArrayList<>();
@@ -46,22 +47,24 @@ public class FrozenTubeCheckService {
             }
             stringList.add(0,sampleCode);stringList.add(1,sampleClassificationCode);
             FrozenTube frozenTube = frozenTubeMap.get(stringList);
-            if(frozenTube.getSampleClassification()!=null&&tube.getSampleClassification()!=null){
-                if((frozenTube.getId()==null && frozenTube.getSampleClassification().getId()==tube.getSampleClassification().getId())
-                    ||(frozenTube.getId()!=null
-                    && frozenTube.getId()!=tube.getId()
-                    &&!frozenTube.getId().equals(tube.getId())
-                    &&frozenTube.getSampleClassification().getId()!=tube.getSampleClassification().getId()
-                    &&!frozenTube.getSampleClassification().getId().equals(tube.getSampleClassification().getId()))){
-                    frozenTubeListForCheckRepeat.add(frozenTube);
-                }
-            }else{//在给项目必须配置样本分类以后不会出现这样情况
-                if(frozenTube.getId()==null
-                    || (frozenTube.getId() != null && frozenTube.getId()!=tube.getId()
-                    && !frozenTube.getId().equals(tube.getId()))){
-                    frozenTubeListForCheckRepeat.add(frozenTube);
+            if(frozenTube != null){
+                if(frozenTube.getSampleClassification()!=null&&tube.getSampleClassification()!=null){//有分类
+                    //新增时，分类相同不能保存
+                    if(frozenTube.getId()==null&&frozenTube.getSampleClassification().getId()==tube.getSampleClassification().getId()){
+                        frozenTubeListForCheckRepeat.add(frozenTube);
+                    }
+                    //修改时，分类相同，ID不同，不能保存
+                    if(frozenTube.getId()!=null&&!frozenTube.getId().equals(tube.getId())
+                        &&frozenTube.getSampleClassification().getId().equals(tube.getSampleClassification().getId())){
+                        frozenTubeListForCheckRepeat.add(frozenTube);
+                    }
+                }else{//无分类  在给项目必须配置样本分类以后不会出现这样情况
+                    if(frozenTube.getId()==null || (frozenTube.getId() != null && frozenTube.getId()!=tube.getId())){
+                        frozenTubeListForCheckRepeat.add(frozenTube);
+                    }
                 }
             }
+
         }
         JSONArray jsonArray = new JSONArray();
         for(FrozenTube f:frozenTubeListForCheckRepeat){

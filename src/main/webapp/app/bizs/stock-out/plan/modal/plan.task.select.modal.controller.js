46c00a140aae03325a4b70f0ebc5a1ff85486021
gaokangkang <kangkang.gao@fwoxford.org>
2017-12-13 15:35:51 +0800

    计划修改

--- src/main/webapp/app/bizs/stock-out/plan/modal/plan.task.select.modal.controller.js
+++ src/main/webapp/app/bizs/stock-out/plan/modal/plan.task.select.modal.controller.js
@@ -12,28 +12,38 @@
 
     function PlanTaskSelectModalController($uibModalInstance,$uibModal,items,toastr,DTOptionsBuilder,PlanService,BioBankDataTable) {
         var vm = this;
-        var paginationIndexArray = [];
+        var indexArray = [];
+        var _indexs = [];
 
         function _fnVerification() {
             //输入有误
             vm.errorInfo = false;
-            vm.paginationText = _.replace(vm.paginationText, '，', ',');
-            paginationIndexArray = _.split(vm.paginationText, ',');
+            vm.indexText = _.replace(vm.indexText, '，', ',');
+            indexArray = _.split(vm.indexText, ',');
             var rgExp = /\d{1,3}-\d{1,3}/;
-            _.forEach(paginationIndexArray,function (pagination) {
-                var index = pagination.indexOf("-");
+            _.forEach(indexArray,function (indexStr) {
+                var index = indexStr.indexOf("-");
                 //检查输入的内容中是否包含-
                 if(index != -1){
-                    if(rgExp.test(pagination)){
+                    if(rgExp.test(indexStr)){
                         //匹配成功后，判断第一个数要小于第二个数，不然就输入错误
-                        var num1 = _.toNumber(pagination.substr(0,index));
-                        var num2 = _.toNumber(pagination.substring(index+1));
+                        var num1 = _.toNumber(indexStr.substr(0,index));
+                        var num2 = _.toNumber(indexStr.substring(index+1));
+
+
                         if(num1 > num2){
                             vm.errorInfo = true
+                        }else{
+                            for(var i = num1; i<= num2;i++){
+                                _indexs.push(i)
+                            }
+
                         }
                     }else{
                         vm.errorInfo = true;
                     }
+                }else{
+                    _indexs.push(Number(indexStr));
                 }
             });
         }
@@ -47,7 +57,8 @@
                 toastr.error("输入有误!");
                 return;
             }
-            $uibModalInstance.close(paginationIndexArray);
+            _indexs = _.union(_indexs);
+            $uibModalInstance.close(_indexs);
 
         };
         vm.cancel = function () {
